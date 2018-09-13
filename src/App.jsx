1681eec884e9006a9e1d0e7c6f9d05252bb55628
Carol Wang <carolmwang@gmail.com>
2018-09-13 15:28:01 -0400

    added user pass for 2nd project

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ class App extends Component {
         <h1>Carol Wang</h1>
           <div><a href="https://carolmwang.github.io/unit01Project/">Hide &amp; Seek</a></div>
           <div><a href="https://hidden-bastion-17613.herokuapp.com/auth/login">Park Advisor</a></div>
+          <p>username: cwang</p>
+          <p>password: pass</p>
           <div><a href="http://shy-loss.surge.sh">Fetch</a></div>
           <div><a href="http://wooden-soup.surge.sh/">Points of Interest</a></div>
       </div>
