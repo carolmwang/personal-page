e437ceba0829d0dadfa7099d9542e1666c661d95
Carol Wang <carolmwang@gmail.com>
2018-09-13 15:23:21 -0400

    Initialized repo, setup react-app and setup gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Carol Wang</h1>
+          <div><a href="https://carolmwang.github.io/unit01Project/">Hide &amp; Seek</a></div>
+          <div><a href="https://hidden-bastion-17613.herokuapp.com/auth/login">Park Advisor</a></div>
+          <div><a href="http://shy-loss.surge.sh">Fetch</a></div>
+          <div><a href="http://wooden-soup.surge.sh/">Points of Interest</a></div>
+      </div>
+    );
+  }
+}
+export default App;
