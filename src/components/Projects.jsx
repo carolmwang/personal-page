6e4da101304bf02ebcd35e7d7f1bc2b6dd34cd08
Carol Wang <carolmwang@gmail.com>
2018-09-25 18:26:36 -0400

    style changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,13 +7,14 @@ export default function Footer() {
         <h1 className="headers"><strong>PROJECTS</strong></h1>
         <br />
         <br />
+        <br />
 
         <div className="columns is-centered">
           <a href="https://carolmwang.github.io/unit01Project/" style={{color: 'black'}}>
             <div className="column project">
               <h1 className="project-title"><strong>Hide &amp; Seek</strong></h1>
               <br />
-
+              <br />
               <img src="https://i.imgur.com/AQl9Szbm.png" alt="" />
             </div>
           </a>
@@ -22,15 +23,18 @@ export default function Footer() {
             <div className="column project">
               <h1 className="project-title"><strong>Park Advisor</strong></h1>
               <br />
+              <br />
               <img src="https://i.imgur.com/1ct59bKm.png" alt="" />
+              <br />
             </div>
           </a>
         </div>
-
+        <br />
         <div className="columns is-centered">
           <a href="http://unequaled-profit.surge.sh/" style={{color: 'black'}}>
             <div className="column project">
               <h1 className="project-title"><strong>Fetch</strong></h1>
+              <br />
               <br />
               <img src="https://i.imgur.com/wRY4lkkm.png" alt="" />
             </div>
@@ -40,7 +44,9 @@ export default function Footer() {
             <div className="column project">
               <h1 className="project-title"><strong>Points of Interest</strong></h1>
               <br />
+              <br />
               <img src="https://i.imgur.com/JUeVFlqm.png" alt="" />
+              <br />
             </div>
           </a>
         </div>
